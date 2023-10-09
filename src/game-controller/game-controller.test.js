d12e2e94ac319f084be7a8d1ca5d9a88637bf25f
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2023-10-09 09:20:39 +0530

    Update GameController test cases

--- src/game-controller/game-controller.test.js
+++ src/game-controller/game-controller.test.js
@@ -4,6 +4,7 @@ const controller = GameController();
 
 test("Should update shipPlacement object with property ship type and value ship coordinates", () => {
   controller.placeShips("carrier", [0, 0]);
+  controller.placeShips("patrolBoat", [9, 0]);
 
   expect(controller.gameBoard.getShipPlacements()).toEqual({
     carrier: {
@@ -16,16 +17,65 @@ test("Should update shipPlacement object with property ship type and value ship 
       ],
       cellsTaken: [1, 2, 3, 4, 5],
     },
+    patrolBoat: {
+      coordinates: [
+        [9, 0],
+        [9, 1],
+      ],
+      cellsTaken: [91, 92],
+    },
   });
 });
 
-// test("Ship coordinates length should equal to ship's length");
+test("Should not place ships from outside of game board", () => {
+  expect(controller.placeShips("battleship", [0, 9])).toBe(
+    "Cannot place ship outside of game board",
+  );
+  expect(controller.placeShips("destroyer", [-1, 4])).toBe(
+    "Cannot place ship outside of game board",
+  );
+  expect(controller.placeShips("submarine", [2, 10])).toBe(
+    "Cannot place ship outside of game board",
+  );
+});
 
 test("Should not update shipsPlacement object if ship is outside of game board", () => {
   controller.placeShips("battleship", [0, 9]);
+  controller.placeShips("destroyer", [-1, 4]);
+  controller.placeShips("submarine", [2, 10]);
+
   expect(
     Object.hasOwn(controller.gameBoard.getShipPlacements(), "battleship"),
   ).toBeFalsy();
+
+  expect(
+    Object.hasOwn(controller.gameBoard.getShipPlacements(), "destroyer"),
+  ).toBeFalsy();
+
+  expect(
+    Object.hasOwn(controller.gameBoard.getShipPlacements(), "submarine"),
+  ).toBeFalsy();
 });
 
-// test("Should not place ships on top of each other");
+test("Should not place ships on top of each other", () => {
+  expect(controller.placeShips("destroyer", [0, 1])).toBe(
+    "Cannot place ship space already acquired",
+  );
+
+  expect(controller.placeShips("submarine", [9, 1])).toBe(
+    "Cannot place ship space already acquired",
+  );
+});
+
+test("Should not update shipsPlacement object if space already acquired", () => {
+  controller.placeShips("destroyer", [0, 1]);
+  controller.placeShips("submarine", [9, 1]);
+
+  expect(
+    Object.hasOwn(controller.gameBoard.getShipPlacements(), "destroyer"),
+  ).toBeFalsy();
+
+  expect(
+    Object.hasOwn(controller.gameBoard.getShipPlacements(), "submarine"),
+  ).toBeFalsy();
+});
