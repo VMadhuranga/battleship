671cacd929b8a4eddbc4db9975bcd54c6e6c3760
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2023-10-11 11:12:09 +0530

    Refactor game-board.js
    Remove getShipPlacements, updateShipPlacements,
    checkAvailableShipPlacements, checkShipAttacked, updateMissedAttacks,
    updateHitAttacks, getMissedAttacks, getHitAttacks functions in
    GameBoard factory
    
    Remove shipPlacements, hitAttacks, missedAttacks variables in
    GameBoard factory

--- src/game-board/game-board.js
+++ src/game-board/game-board.js
@@ -1,8 +1,5 @@
 const GameBoard = () => {
   const board = [];
-  const shipPlacements = {};
-  const hitAttacks = new Map();
-  const missedAttacks = new Map();
 
   function createGameBoard() {
     let cell = 1;
@@ -20,46 +17,10 @@ const GameBoard = () => {
 
   const getBoard = () => board;
   const getBoardCell = (index) => board?.[index[0]]?.[index[1]];
-  const getShipPlacements = () => shipPlacements;
-
-  const updateShipPlacements = (shipType, coordinates, cellsTaken) => {
-    shipPlacements[shipType] = { coordinates, cellsTaken };
-  };
-
-  const checkAvailableShipPlacements = (cellsNeeded) =>
-    !Object.keys(shipPlacements).some((key) =>
-      shipPlacements[key].cellsTaken.some((taken) =>
-        cellsNeeded.includes(taken),
-      ),
-    );
-
-  const checkShipAttacked = (coordinates) =>
-    Object.keys(shipPlacements).find((key) =>
-      shipPlacements[key].cellsTaken.includes(getBoardCell(coordinates)),
-    );
-
-  const updateMissedAttacks = (coordinates) => {
-    missedAttacks.set(getBoardCell(coordinates), coordinates);
-  };
-
-  const getMissedAttacks = () => missedAttacks;
-  const getHitAttacks = () => hitAttacks;
-
-  const updateHitAttacks = (coordinates) => {
-    hitAttacks.set(getBoardCell(coordinates), coordinates);
-  };
 
   return {
     getBoard,
     getBoardCell,
-    getShipPlacements,
-    updateShipPlacements,
-    checkAvailableShipPlacements,
-    checkShipAttacked,
-    updateMissedAttacks,
-    updateHitAttacks,
-    getMissedAttacks,
-    getHitAttacks,
   };
 };
 
