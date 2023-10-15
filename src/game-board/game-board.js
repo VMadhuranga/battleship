4dcdb11ec5040944bf0a7ce165cd058a6a6fa52d
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2023-10-15 09:06:23 +0530

    Update game-board.js
    Create getBoardCellCoordinates function in GameBoard factory

--- src/game-board/game-board.js
+++ src/game-board/game-board.js
@@ -18,9 +18,19 @@ const GameBoard = () => {
   const getBoard = () => board;
   const getBoardCell = (index) => board?.[index[0]]?.[index[1]];
 
+  const getBoardCellCoordinates = (cell) =>
+    board.reduce((acc, val, index) => {
+      if (val.indexOf(cell) > -1) {
+        acc.push(index, val.indexOf(cell));
+      }
+
+      return acc;
+    }, []);
+
   return {
     getBoard,
     getBoardCell,
+    getBoardCellCoordinates,
   };
 };
 
